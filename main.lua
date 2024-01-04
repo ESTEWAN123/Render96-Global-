c1012e51218e4fd8144f0aa3a00ec719228ea98f
EpikNames <113206896+ESTEWAN123@users.noreply.github.com>
2024-01-04 12:03:26 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,35 @@
+-- name: \\#08f26e\\Render96 \\#dcdcdc\\Global
+-- description: Its About The Render model that you can See Globaly That All Can see the render Render And all things to:Render96 team \n\Owner:EpikNames\n\LF64:Cool guy
+-- incompatible: skin pack
+
+E_MODEL_MARIO_GEO = smlua_model_util_get_id("mario_geo") or E_MODEL_MARIO
+E_MODEL_LUIGI_GEO = smlua_model_util_get_id("luigi_geo") or E_MODEL_LUIGI
+E_MODEL_WALUIGI_GEO = smlua_model_util_get_id("waluigi_geo") or E_MODEL_WALUIGI
+E_MODEL_TOAD_PLAYER_GEO = smlua_model_util_get_id("toad_player_geo") or E_MODEL_TOAD_PLAYER
+E_MODEL_WARIOS_GEO = smlua_model_util_get_id("wario_geo") or E_MODEL_WARIO
+E_MODEL_MARIOS_CAP_GEO = smlua_model_util_get_id("marios_cap_geo") or E_MODEL_MARIOS_CAP
+E_MODEL_WARIO_CAP_GEO = smlua_model_util_get_id("warios_cap_geo") or E_MODEL_WARIOS_CAP
+E_MODEL_LUIGIS_CAP_GEO = smlua_model_util_get_id("luigis_cap_geo") or E_MODEL_LUIGIS_CAP
+E_MODEL_WALUIGIS_CAP_GEO = smlua_model_util_get_id("waluigis_cap_geo") or E_MODEL_WALUIGIS_CAP
+E_MODEL_MARIOS_WINGED_METAL_CAP_GEO = smlua_model_util_get_id("marios_winged_metal_cap_geo") or E_MODEL_MARIOS_WINGED_METAL_CAP
+E_MODEL_WARIOS_WINGED_METAL_CAP_GEO = smlua_model_util_get_id("warios_winged_metal_cap_geo") or E_MODEL_WARIOS_WINGED_METAL_CAP
+E_MODEL_LUIGIS_WINGED_METAL_CAP_GEO = smlua_model_util_get_id("luigis_winged_metal_cap_geo") or E_MODEL_LUIGIS_WINGED_METAL_CAP
+E_MODEL_WALUIGIS_WINGED_METAL_CAP_GEO = smlua_model_util_get_id("waluigis_winged_metal_cap_geo") or E_MODEL_WALUIGIS_WINGED_METAL_CAP
+E_MODEL_STAR_GEO = smlua_model_util_get_id("star_geo") or E_MODEL_STAR
+E_MODEL_YELLOW_GEO = smlua_model_util_get_id("yellow_coin_geo") or E_YELLOW_MODEL_COIN
+E_MODEL_BLUE_COIN_GEO = smlua_model_util_get_id("blue_coin_geo") or E_MODEL_BLUE_COIN
+E_MODEL_WOODEN_SIGNPOST_GEO = smlua_model_util_get_id("altwooden_signpost_geo") or E_MODEL_WOODEN_SIGNPOST
+E_MODEL_TRANSPARENT_STAR_GEO = smlua_model_util_get_id("transparent_star_geo") or E_MODEL_TRANSPARENT_STAR
+E_MODEL_RED_COIN_GEO = smlua_model_util_get_id("red_coin_geo") or E_MODEL_RED_COIN
+E_MODEL_MUSHROOM_1UP_GEO = smlua_model_util_get_id("mushroom_1up_geo") or E_MODEL_MUSHROOM_1UP
+E_MODEL_WHOMP_GEO = smlua_model_util_get_id("whomp_geo") or E_MODEL_WHOMP
+E_MODEL_WARP_PIPE_GEO = smlua_model_util_get_id("warp_pipe_geo") or E_MODEL_WARP_PIPE
+E_MODEL_WOODEN_DOOR_GEO = smlua_model_util_get_id("wooden_door_geo") or E_MODEL_WOODEN_DOOR
+E_MODEL_MIPS_GEO = smlua_model_util_get_id("mips_geo") or E_MODEL_MIPS
+E_MODEL_CASTLE_DOOR_GEO = smlua_model_util_get_id("castle_door_geo") or E_MODEL_CASTLE_DOOR
+E_MODEL_CASTLE_DOOR_1_GEO = smlua_model_util_get_id("castle_door_1_geo") or E_MODEL_CASTLE_1_DOOR
+E_MODEL_CASTLE_DOOR_2_GEO = smlua_model_util_get_id("castle_door_2_geo") or E_MODEL_CASTLE_2_DOOR
+E_MODEL_CASTLE_DOOR_3_GEO = smlua_model_util_get_id("castle_door_3_geo") or E_MODEL_CASTLE_3_DOOR
+end
+
+
